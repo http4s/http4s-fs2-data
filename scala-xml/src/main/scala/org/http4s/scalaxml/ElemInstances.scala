8addd0fd82aad9b7a7fa74705b3e250879df936a
Yannick Heiber <yannick.heiber@mobimeo.com>
2022-09-02 13:03:18 +0200

    Implement RFC-compliant encoding detection for XML

--- scala-xml/src/main/scala/org/http4s/scalaxml/ElemInstances.scala
+++ scala-xml/src/main/scala/org/http4s/scalaxml/ElemInstances.scala
@@ -19,14 +19,16 @@ package scalaxml
 
 import cats.effect.Concurrent
 import cats.syntax.all._
-import fs2.Stream
+import fs2.{Chunk, Pull, Stream}
 import fs2.data.xml.XmlEvent
 import fs2.data.xml.XmlException
 import fs2.data.xml.scalaXml._
+import fs2.text.decodeWithCharset
 import org.http4s.Charset.`UTF-8`
 import org.http4s.headers.`Content-Type`
 
 import java.io.StringWriter
+import scala.util.matching.Regex
 import scala.xml.Document
 import scala.xml.Elem
 import scala.xml.XML
@@ -51,7 +53,42 @@ trait ElemInstances {
   )(implicit F: Concurrent[F]): EntityDecoder[F, Stream[F, XmlEvent]] =
     EntityDecoder.decodeBy(MediaType.text.xml, MediaType.text.html, MediaType.application.xml) {
       msg =>
-        DecodeResult.successT(msg.bodyText.through(fs2.data.xml.events(includeComments)))
+        DecodeResult.successT(
+          msg.body.pull
+            .unconsN(64, allowFewer = true)
+            // look at the first bytes to check for BOM or encoding spec in the prolog
+            .flatMap {
+              case Some((chunk, tail)) if chunk.take(2) === `BOM-BE` =>
+                tail
+                  .cons(chunk.drop(2)) // drop BOM
+                  .through(decodeWithCharset(Charset.`UTF-16BE`.nioCharset))
+                  .pull
+                  .echo
+              case Some((chunk, tail)) if chunk.take(2) === `BOM-LE` =>
+                tail
+                  .cons(chunk.drop(2)) // drop BOM
+                  .through(decodeWithCharset(Charset.`UTF-16LE`.nioCharset))
+                  .pull
+                  .echo
+              case Some((chunk, tail)) if msg.charset.isEmpty =>
+                // guard: xml prolog is only authoritative if content type is missing, see RFC 7303 §8.8
+                guessCharset(new String(chunk.toArray, `UTF-8`.nioCharset))
+                  .fold(
+                    Pull.raiseError[F](_),
+                    charset =>
+                      tail.cons(chunk).through(decodeWithCharset(charset.nioCharset)).pull.echo,
+                  )
+              case Some((chunk, tail)) =>
+                tail
+                  .cons(chunk)
+                  .through(decodeWithCharset(msg.charset.getOrElse(`UTF-8`).nioCharset))
+                  .pull
+                  .echo
+              case None => Pull.done
+            }
+            .stream
+            .through(fs2.data.xml.events(includeComments))
+        )
     }
 
   /** Handles a message body as XML.
@@ -82,4 +119,16 @@ trait ElemInstances {
           .widen
       }
     }
+
+  // Extract the encoding from the XML prolog if present. Attribute order is fixed for prologs by the spec
+  // and additional whitespace is not allowed, so this strict regex should work reliably.
+  private val prologWithEncoding: Regex = """<\?xml version=".+" encoding="([^"]+)".*""".r
+
+  private val `BOM-BE` = Chunk(0xfe.toByte, 0xff.toByte)
+  private val `BOM-LE` = Chunk(0xff.toByte, 0xfe.toByte)
+
+  private def guessCharset(in: String): ParseResult[Charset] = in match {
+    case prologWithEncoding(encoding) => Charset.fromString(encoding)
+    case _ => Right(Charset.`UTF-8`)
+  }
 }
